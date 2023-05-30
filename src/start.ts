fc844dfa317cf637242550f93fd5021d7a011cb1
Dinh Bang <ndinhbang111@gmail.com>
2023-05-30 21:44:37 +0700

    vscode settings

--- src/start.ts
+++ src/start.ts
@@ -9,5 +9,6 @@ app.get('/', (_req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
